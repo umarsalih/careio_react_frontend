d6864b621cc945269308f6fb991342b52e5d266b
umarsalih <salihumar13@gmail.com>
2022-11-11 08:30:04 -0800

    Routed routers, foundation for routing setup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import './App.css';
+import Main from "./components/Main";
+import Signup from './components/Signup';
+import { Routes,Route, Link } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Blank Page</h1>
+    <div className='App'>
+      <Routes>
+        <Route path='/' element={<Main/>} />
+        <Route path='/signup' element={<Signup/>} />
+      </Routes>
     </div>
   );
 }
