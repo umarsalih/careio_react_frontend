f767eaf041093e85df5d0f753e61cbb08535fbb6
maccimac <macci.web@gmail.com>
2022-11-12 14:48:34 -0800

    init

---
+++ public/mock-data/user-login.js
@@ -0,0 +1,15 @@
+{
+    status_msg: 'success',
+    login:{
+        login_id: ,
+        user_id: ,
+        token: ,
+    }
+    user: {
+        user_id: ,
+        first_name: ,
+        last_name: ,
+        phone: ,
+        address: ,
+    }
+}
