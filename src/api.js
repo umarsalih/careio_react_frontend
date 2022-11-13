84b3890de10f3845fea6708d0f08cc33fb871ea9
maccimac <macci.web@gmail.com>
2022-11-12 19:51:24 -0800

    more api tests

--- src/api.js
+++ src/api.js
@@ -1,6 +1,8 @@
 // Booking
-export const createBooking = () => {
-    const bodySample ={
+import axios from 'axios';
+
+export const createBooking = async (bookingObj, cb) => {
+    if(!bookingObj) bookingObj ={
         "userId": 10,
         "serviceProviderId": 21,
         "bookingDate": "01-04-2023",
@@ -12,23 +14,48 @@ export const createBooking = () => {
     }
 
     try{
-        console.log(JSON.stringify(bodySample))
-
-        fetch(`${Config.HOST}/booking`, {
+        const options = {
+            url: 'http://localhost:8080/booking/',
             method: 'POST',
-            credentials:'include',
-            headers: {
+            headers: { 
                 'Content-Type': 'application/json',
                 'Access-Control-Allow-Origin': '*',
                 'Authorization' : "Basic cm9vdDpyb290",
                 'Access-Control-Allow-Methods': 'DELETE, POST, GET, OPTIONS',
                 'Access-Control-Allow-Headers':'Content-Type, Authorization, X-Requested-With'
             },
-            body: JSON.stringify(bodySample),
+            body: bookingObj,
             json: true 
-        })
+            };
+        const res = await axios(options);
+        if(cb) cb()
+        return res.data
     } catch(e){
-        consele.error(`[careio::error] ${e.message}`)
+        console.error(`[careio::error] ${e.message}`)
+        return {}
     }
 
+}
+
+export const getBookings = async(cb) => {
+    console.log('[getBookings]')
+    try{
+        const options = {
+            method: 'GET',
+            headers: { 
+                'Content-Type': 'application/json',
+                'Access-Control-Allow-Origin': '*',
+                'Authorization' : "Basic cm9vdDpyb290",
+                'Access-Control-Allow-Methods': 'DELETE, POST, GET, OPTIONS',
+                'Access-Control-Allow-Headers':'Content-Type, Authorization, X-Requested-With'
+            },
+            json: true,
+            url: 'http://localhost:8080/booking/',
+            };
+        const res = await axios(options);
+        console.log({res})
+        if(cb) cb()
+    } catch(e) {
+        console.error(`[careio::error] ${e}`)
+    }
 }
--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,73 +1,26 @@
-import React, { useState, useEffect, Fragment} from 'react';
+import React, { useRef, useState, useEffect, Fragment} from 'react';
 import { Link } from 'react-router-dom';
 import Hero from '../home/Hero'
 import TabsPartners from './../home/TabsPartners'
 import BookingModal from './../booking/BookingModal'
 import LoginModal from './../login/LoginModal'
 import axios from 'axios';
+import {getBookings} from './../../api'
 
 const Home = () => {
-    let fetched = false
-    const testFetch = async() => {
-        const data = {
-            key: 'val'
-        }
-        try{
-           
-                // fetch(`http://localhost:8080/booking`, {
-                //     method: 'GET',
-                //     credentials:'include',
-                //     headers: {
-                //         'Content-Type': 'application/json',
-                //         'Access-Control-Allow-Origin': '*',
-                //         'Authorization' : "Basic cm9vdDpyb290",
-                //         'Access-Control-Allow-Methods': 'DELETE, POST, GET, OPTIONS',
-                //         'Access-Control-Allow-Headers':'Content-Type, Authorization, X-Requested-With'
-                //     },
-                //     json: true 
-                // }).then(res=>{
-                //     console.log(res)
-                //     return res.json()
-                // }).then( data => {
-                //     console.log('getBookings', {data})
-                // })
-                
-        // // data: qs.stringify(data),
-            const options = {
-                method: 'GET',
-                headers: { 
-                    'Content-Type': 'application/json',
-                    'Access-Control-Allow-Origin': '*',
-                    'Authorization' : "Basic cm9vdDpyb290",
-                    'Access-Control-Allow-Methods': 'DELETE, POST, GET, OPTIONS',
-                    'Access-Control-Allow-Headers':'Content-Type, Authorization, X-Requested-With'
-                },
-                json: true,
-                url: 'http://localhost:8080/booking/',
-                };
-            const res = await axios(options);
+    // let fetched = useRef(false)
+    console.log(
+        Date.parse('10-11-2022 07:00')
+    )
 
-            // const res = await axios.post('http://localhost:8080/booking/', {
-            //     method: 'POST',
-            //     credentials:'include',
-            //     headers: {
-            //         'Content-Type': 'application/json',
-            //         'Access-Control-Allow-Origin': '*',
-            //         'Authorization' : "Basic cm9vdDpyb290",
-            //         'Access-Control-Allow-Methods': 'DELETE, POST, GET, OPTIONS',
-            //         'Access-Control-Allow-Headers':'Content-Type, Authorization, X-Requested-With'
-            //     },
-            //     json: true 
-            // })
-            
-            fetched = true
-            console.log({res})
-        } catch(e) {
-            console.error(`[careio::error] ${e}`)
-        }
-    }
+
+    
     useEffect(()=>{
-        if(!fetched) testFetch()
+        // if(fetched) return
+        // const newBooking = getBookings( ()=>{
+        //     fetched = true
+        // })
+        // console.log({newBooking})
     },[])
     
     return (
