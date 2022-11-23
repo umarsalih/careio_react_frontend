769381724716b58573fb8773e13485152f4dbcbc
umarsalih <salihumar13@gmail.com>
2022-11-22 22:00:56 -0800

    Updating text on signup page

--- src/components/pages/Signup-info.jsx
+++ src/components/pages/Signup-info.jsx
@@ -2,47 +2,96 @@ import React from 'react'
 // import "./../../styles/style.css";
 // import "./../../styles/layout-signup.css";
 
-function SignupInfo() {
-    return (
-        <div
-            className="signupInfo col-12 col-sm-12 col-md-4 col-lg-6 order-sm-2 order-md-1">
-            <div className='holder' >
-                <div class="signupInfo-headertext">
-                    <h1>Sign up as a client <br/>
-                        
-                        with Care.io</h1>
-                </div>
+function SignupInfo(props) {
 
-                <div className="signupInfo-textbox1">
-                    <h5 className='signup-subheading'>What you get as a free-tier client?</h5>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore eligendi quod
-                        nobis hic dolore consequuntur aut officiis ad nulla dolorem.</p>
+    const isClientView = props.isClientView;
 
-                    <ul>
-                        <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Et, velit.</li>
-                        <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Et, velit.</li>
-                        <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Et, velit.</li>
-                        <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Et, velit.</li>
-                    </ul>
-                </div>
-
-                <div className="signupInfo-textbox2">
-                    <h5 className='signup-subheading'>Why go
-                            premium?</h5>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore eligendi quod
-                        nobis hic dolore consequuntur aut officiis ad nulla dolorem.</p>
-
-                    <ul>
-                        <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Et, velit.</li>
-                        <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Et, velit.</li>
-                        <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Et, velit.</li>
-                        <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Et, velit.</li>
-                    </ul>
+    if(isClientView == true) {
+        return (
+            <div
+                className="signupInfo col-12 col-sm-12 col-md-4 col-lg-6 order-sm-2 order-md-1">
+                <div className='holder' >
+                    <div class="signupInfo-headertext">
+                        <h1>Sign up as a client <br/>
+                            
+                            with Care.io</h1>
+                    </div>
+                    
+    
+                    <div className="signupInfo-textbox1">
+                        <h5 className='signup-subheading'>What you get as a free-tier client?</h5>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore eligendi quod
+                            nobis hic dolore consequuntur aut officiis ad nulla dolorem.</p>
+    
+                        <ul>
+                            <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Et, velit.</li>
+                            <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Et, velit.</li>
+                            <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Et, velit.</li>
+                            <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Et, velit.</li>
+                        </ul>
+                    </div>
+    
+                    <div className="signupInfo-textbox2">
+                        <h5 className='signup-subheading'>Why go
+                                premium?</h5>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore eligendi quod
+                            nobis hic dolore consequuntur aut officiis ad nulla dolorem.</p>
+    
+                        <ul>
+                            <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Et, velit.</li>
+                            <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Et, velit.</li>
+                            <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Et, velit.</li>
+                            <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Et, velit.</li>
+                        </ul>
+                    </div>
                 </div>
             </div>
-        </div>
-
-    )
+    
+        )
+    }
+    else {
+        return (
+            <div
+                className="signupInfo col-12 col-sm-12 col-md-4 col-lg-6 order-sm-2 order-md-1">
+                <div className='holder' >
+                    <div class="signupInfo-headertext">
+                        <h1>Sign up as a service partner <br/>
+                            
+                            with Care.io</h1>
+                    </div>
+                    
+    
+                    <div className="signupInfo-textbox1">
+                        <h5 className='signup-subheading'>What you get as a free-tier service partner?</h5>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore eligendi quod
+                            nobis hic dolore consequuntur aut officiis ad nulla dolorem.</p>
+    
+                        <ul>
+                            <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Et, velit.</li>
+                            <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Et, velit.</li>
+                            <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Et, velit.</li>
+                            <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Et, velit.</li>
+                        </ul>
+                    </div>
+    
+                    <div className="signupInfo-textbox2">
+                        <h5 className='signup-subheading'>Why go
+                                premium?</h5>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore eligendi quod
+                            nobis hic dolore consequuntur aut officiis ad nulla dolorem.</p>
+    
+                        <ul>
+                            <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Et, velit.</li>
+                            <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Et, velit.</li>
+                            <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Et, velit.</li>
+                            <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Et, velit.</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+    
+        )
+    }
 }
 
 export default SignupInfo
--- src/components/pages/Signup.jsx
+++ src/components/pages/Signup.jsx
@@ -6,16 +6,24 @@ import SignupForm from './Signup-form';
 import { useSelector } from 'react-redux';
 import { isLoggedIn } from '../../store/login-store';
 import { Link } from 'react-router-dom'
+import {useState} from 'react'
 
 function Signup() {
     const isLogged = useSelector(isLoggedIn);
+    const [isClientView, setClientView] = useState(true);
+
     return (
         <div id="CareIOSignup">
             <div className='careio_superwrapper'>
                 <section id="container-01" className="container-fluid" x-class="section-full">
                     <div className='row'>
-                        <SignupInfo/>
-                        <SignupForm/>
+                        <SignupInfo
+                            isClientView={isClientView}
+                        />
+                        <SignupForm
+                            isClientView={isClientView}
+                            setClientView={setClientView}
+                        />
                     </div>
                 </section>
             </div>
